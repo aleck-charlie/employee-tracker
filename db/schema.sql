07aa5ce91d350eebd69697f24d1effe33c3d97d8
Charlie Aleck <aleckcharlie@gmail.com>
2021-06-23 11:27:04 -0500

    populated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,25 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+CREATE TABLE department (
+    id INT not NULL PRIMARY KEY DEFAULT 0,
+    name VARCHAR(30) not NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT not NULL PRIMARY KEY,
+    first_name VARCHAR(30) not NULL,
+    last_name VARCHAR(30) not NULL,
+    role_id INT not NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+CREATE TABLE role (
+    id INT not NULL PRIMARY KEY DEFAULT 0,
+    title VARCHAR(30) not NULL,
+    salary DECIMAL(10,2) not NULL,
+    department_id INT not NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
+);
