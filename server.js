3eb5a9f433cc79c973f3990add39a578cc2c6804
Charlie Aleck <aleckcharlie@gmail.com>
2021-06-23 12:04:53 -0500

    setting up server

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 require('console.table');
+
+// loadprompts (inquirer choices)
+// selecting a choice will go to that function
+// view all employees
+// view employees by department
+// view employees by manager
+// add employee
+// remove emplyee
+// update employee role
+// update employee manager
+// view roles
+// add role
+// remove role
+// view departments
+// add department
+// remove department
+// quit
